24b0c6d98ca59d6f39176a48ef42c20fe79e5783
Saurav Bhasin <128213153+bhasinsaurav@users.noreply.github.com>
2025-08-11 19:27:18 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"). // we are using node js , not brows
+
+let name = "Saurav"
+
+let age = 22
+
+let isLoggedIn = false
+
+/**
+ * Types of DataTypes
+ * number
+ * BigInt
+ * String
+ * boolean
+ * null -> it is a standalone value
+ * undefined -> 
+ * symbol -> best for uniqueness
+ * object
+ */
+
+
+console.log(typeof undefined) // undefined
+
+console.log(typeof null) //object
