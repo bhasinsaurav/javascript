63b2e764f88a632c5ae623034d7a9f037cf74e69
Saurav Bhasin <128213153+bhasinsaurav@users.noreply.github.com>
2025-08-20 19:00:54 +0000

    Date and time

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,27 @@
+let myDate = new Date();
+// console.log(myDate);
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.getFullYear());
+
+let myCreatedDate = new Date(2023, 0, 23);
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(myTimeStamp / 1000)); // Convert to seconds
+
+let 
+
+
+
+
+
